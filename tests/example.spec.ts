413387eaf573d2bebac11ad05c7ae5a53af14035
Rob Martin <robert.martin@gmsl.co.uk>
2023-11-30 07:55:03 +0000

    Added tests for login

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,26 @@
 import { test, expect } from "@playwright/test";
 
-test("First test", async ({ page }) => {
+test("Test mock log in", async ({ page }) => {
+  // go to unuthorised landing page (login)
   await page.goto("/");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Dashboard - Chorus/);
+  // check at authorised landing page (dashboard)
+  await expect(page).toHaveTitle("Dashboard - Chorus");
+});
+
+test("Test log in", async ({ page }) => {
+  // clear any stored tokens
+  await page.evaluate(() => window.localStorage.clear());
+  await page.evaluate(() => window.sessionStorage.clear());
+
+  // go to unuthorised landing page (login)
+  await page.goto("/");
+
+  // enter username & password
+  await page.getByLabel("Username").fill(process.env.PLAYWRIGHT_USERNAME);
+  await page.getByLabel("Password").fill(process.env.PLAYWRIGHT_PASSWORD);
+  await page.getByRole("button", { name: "Log in" }).click();
+
+  // check at authorised landing page (dashboard)
+  await expect(page).toHaveTitle("Dashboard - Chorus");
 });
