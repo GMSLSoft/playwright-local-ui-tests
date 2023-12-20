0c1d5bada5914ef516ef89d32d3ef9ad4de67bc4
Rob Martin <robert.martin@gmsl.co.uk>
2023-12-20 13:25:32 +0000

    Refactored login tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,11 +1,33 @@
 import { test, expect } from "@playwright/test";
+import { defaultCustomerFeaturesResponse } from "../fixtures/customerFeaturesResponses/defaultResponse";
+import { singleCustomerResponse } from "../fixtures/customerResponses/singleCustomerResponse";
+import { defaultDashboardResponse } from "../fixtures/dashboardResponses/defaultResponse";
+import {
+  getUserCustomersRoute,
+  getAuthRoute,
+  getCurrentGasDateRoute
+} from "../support/endpointRoutes/authenticationRoutes";
+import { getDashboardRoute } from "../support/endpointRoutes/dashboardRoutes";
+import { getCustomerFeaturesRoute } from "../support/endpointRoutes/sharedRoutes";
 
 test("Test mock log in", async ({ page }) => {
-  // go to unuthorised landing page (login)
-  await page.goto("/");
+  // add mock routes
+  await getUserCustomersRoute({ page, json: singleCustomerResponse });
+  await getAuthRoute({ page });
+  await getCustomerFeaturesRoute({
+    page,
+    json: defaultCustomerFeaturesResponse
+  });
+  await getCurrentGasDateRoute({ page });
+  await getDashboardRoute({ page, json: defaultDashboardResponse });
 
-  // check at authorised landing page (dashboard)
+  // navigate to authenticated page state
+  await page.goto("/");
   await expect(page).toHaveTitle("Dashboard - Chorus");
+
+  await expect(page.getByRole("heading", { name: "Page Title" })).toHaveText(
+    "Dashboard"
+  );
 });
 
 test("Test log in", async ({ page }) => {
