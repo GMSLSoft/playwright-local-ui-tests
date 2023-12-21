d2e78cdd2828f5dbe46e59e4716d7600dfb4da65
Rob Martin <robert.martin@gmsl.co.uk>
2023-12-21 12:00:31 +0000

    Separated login tests by file

--- tests/login-credentials.spec.ts
+++ tests/login-credentials.spec.ts
@@ -14,30 +14,6 @@ import { getDashboardRoute } from "../support/endpointRoutes/dashboardRoutes";
 import { getCustomerFeaturesRoute } from "../support/endpointRoutes/sharedRoutes";
 import { waitForPulsatingDotsToNotExist } from "../support/pageHelpers/loadingIndicators";
 
-test.beforeEach(async ({ page }) => {
-  // add mock routes
-  await getUserCustomersRoute({ page, json: singleCustomerResponse });
-  await getAuthRoute({ page });
-  await getCustomerFeaturesRoute({
-    page,
-    json: defaultCustomerFeaturesResponse
-  });
-  await getCurrentGasDateRoute({ page });
-  await getDashboardRoute({ page, json: defaultDashboardResponse });
-});
-
-test("Test mock log in", async ({ page }) => {
-  // navigate to authenticated page state
-  await page.goto("/");
-  await waitForPulsatingDotsToNotExist({ page });
-
-  await expect(page).toHaveTitle("Dashboard - Chorus");
-
-  await expect(page.getByRole("heading", { name: "Page Title" })).toHaveText(
-    "Dashboard"
-  );
-});
-
 test("Test successful log in", async ({ browser }) => {
   const blankStorageState = {
     cookies: [],
---
+++ tests/login-token.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from "@playwright/test";
+import { defaultCustomerFeaturesResponse } from "../fixtures/customerFeaturesResponses/defaultResponse";
+import { singleCustomerResponse } from "../fixtures/customerResponses/singleCustomerResponse";
+import { defaultDashboardResponse } from "../fixtures/dashboardResponses/defaultResponse";
+import {
+  getUserCustomersRoute,
+  getAuthRoute,
+  getCurrentGasDateRoute
+} from "../support/endpointRoutes/authenticationRoutes";
+import { getDashboardRoute } from "../support/endpointRoutes/dashboardRoutes";
+import { getCustomerFeaturesRoute } from "../support/endpointRoutes/sharedRoutes";
+import { waitForPulsatingDotsToNotExist } from "../support/pageHelpers/loadingIndicators";
+
+test.beforeEach(async ({ page }) => {
+  // add mock routes
+  await getUserCustomersRoute({ page, json: singleCustomerResponse });
+  await getAuthRoute({ page });
+  await getCustomerFeaturesRoute({
+    page,
+    json: defaultCustomerFeaturesResponse
+  });
+  await getCurrentGasDateRoute({ page });
+  await getDashboardRoute({ page, json: defaultDashboardResponse });
+});
+
+test("Test mock log in", async ({ page }) => {
+  // navigate to authenticated page state
+  await page.goto("/");
+  await waitForPulsatingDotsToNotExist({ page });
+
+  await expect(page).toHaveTitle("Dashboard - Chorus");
+
+  await expect(page.getByRole("heading", { name: "Page Title" })).toHaveText(
+    "Dashboard"
+  );
+});
