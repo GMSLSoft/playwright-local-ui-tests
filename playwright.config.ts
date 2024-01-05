ec2fc7b142941da706327c2cff40d910d063b347
Rob Martin <robert.martin@gmsl.co.uk>
2024-01-05 08:50:53 +0000

    Set browser context default to UTC

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,8 @@ export default defineConfig({
     baseURL: "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry"
+    trace: "on-first-retry",
+    timezoneId: "Etc/UTC"
   },
 
   /* Configure projects for major browsers */
