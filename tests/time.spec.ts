2e4e97816375e4573765c01118c3f4ec01acd96e
Rob Martin <robert.martin@gmsl.co.uk>
2024-01-05 12:31:02 +0000

    Update time spec to use sinon fake date time

--- tests/time.spec.ts
+++ tests/time.spec.ts
@@ -10,9 +10,11 @@ import {
 import { getDashboardRoute } from "@/support/endpointRoutes/dashboardRoutes";
 import { getCustomerFeaturesRoute } from "@/support/endpointRoutes/sharedRoutes";
 import { waitForPulsatingDotsToNotExist } from "@/support/pageHelpers/loadingIndicators";
+import {
+  setupSinonFakeDateTime,
+  tickSinonFakeDateTime
+} from "@/support/pageHelpers/sinonTimeHelpers";
 import { test, expect } from "@playwright/test";
-import * as path from "path";
-import * as sinon from "sinon";
 
 test.beforeEach(async ({ page, context }) => {
   await getUserCustomersRoute({ page, json: singleCustomerResponse });
@@ -24,14 +26,10 @@ test.beforeEach(async ({ page, context }) => {
   await getCurrentGasDateRoute({ page });
   await getDashboardRoute({ page, json: defaultDashboardResponse });
 
-  // add sinon to all page contexts
-  await context.addInitScript({
-    path: path.join(__dirname, "..", "./node_modules/sinon/pkg/sinon.js")
+  await setupSinonFakeDateTime({
+    context,
+    dateTimeString: dateString.plus0.iso
   });
-
-  await context.addInitScript(dateString => {
-    window.__clock = sinon.useFakeTimers(new Date(dateString));
-  }, dateString.plus0.iso);
 });
 
 test("Test moving time", async ({ page }) => {
@@ -45,7 +43,7 @@ test("Test moving time", async ({ page }) => {
   ).toHaveText("Last updated 00:00");
 
   const oneMinute = 1000 * 60;
-  await tick({ page, tickInMilliseconds: oneMinute });
+  await tickSinonFakeDateTime({ page, tickInMilliseconds: oneMinute });
 
   await page.waitForResponse("**/dashboard*");
   await waitForPulsatingDotsToNotExist({ page });
@@ -55,7 +53,7 @@ test("Test moving time", async ({ page }) => {
   ).toHaveText("Last updated 00:01");
 
   const oneHour = oneMinute * 60;
-  await tick({ page, tickInMilliseconds: oneHour });
+  await tickSinonFakeDateTime({ page, tickInMilliseconds: oneHour });
 
   await page.waitForResponse("**/dashboard*");
   await waitForPulsatingDotsToNotExist({ page });
